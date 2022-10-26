8f9948c45d5b45eaec2efc258b5334d1bc5604ec
GhostoftheMill <kevinmichaelwhite@gmail.com>
2022-10-25 23:24:04 -0400

    Worked on creating Today Card in JS

--- Assets/script.js
+++ Assets/script.js
@@ -56,7 +56,64 @@ function fetchWeather(location) {
     	});
 
 }
+function renderHistory(searchForm, )
+/*
+function renderTodayForecast(dailyForecast, timezone) {
 
+  var headingCol = document.createElement('div');
+  var heading = document.createElement('h4');
+
+  headingCol.setAttribute('class', 'col-12');
+  heading.textContent = 'Today:';
+  headingCol.append(heading);
+
+  todayContainer.innerHTML = '';
+  todayContainer.append(headingCol);
+}
+
+function renderTodayCard(forecast, timezone) {
+  // variables for data from api
+  var unixTs = forecast.dt;
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  var iconDescription = forecast.weather[0].description;
+  var tempF = forecast.temp.day;
+  var { humidity } = forecast;
+  var windMph = forecast.wind_speed;
+
+  // Create elements for a card
+  var col = document.createElement('div');
+  var card = document.createElement('div');
+  var cardBody = document.createElement('div');
+  var cardTitle = document.createElement('h5');
+  var weatherIcon = document.createElement('img');
+  var tempEl = document.createElement('p');
+  var windEl = document.createElement('p');
+  var humidityEl = document.createElement('p');
+
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+  col.setAttribute('class', 'col-md');
+  col.classList.add('five-day-card');
+  card.setAttribute('class', 'card bg-primary h-100 text-white');
+  cardBody.setAttribute('class', 'card-body p-2');
+  cardTitle.setAttribute('class', 'card-title');
+  tempEl.setAttribute('class', 'card-text');
+  windEl.setAttribute('class', 'card-text');
+  humidityEl.setAttribute('class', 'card-text');
+
+  // Add content to elements
+  cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', iconDescription);
+  tempEl.textContent = `Temp: ${tempF} °F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+
+  todayContainer.append(col);
+}
+*/
 // Function to display 5 day forecast.
 function renderForecast(dailyForecast, timezone) {
   // Create unix timestamps for start and end of 5 day forecast
@@ -125,10 +182,3 @@ function renderForecastCard(forecast, timezone) {
 
   forecastContainer.append(col);
 }
-
-fetchWeather({
-	lat: 10,
-	lon: 10,
-	name: "San Diego"
-})
-
